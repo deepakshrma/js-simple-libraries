9b4ef7ac20a6bd862f5abfd88fb96cb79ebcf6ea
xdeepakv <deepak.m.shrma@gmail.com>
2019-04-15 02:16:40 +0800

    added react live demo

--- packages/playground/pages/index.tsx
+++ packages/playground/pages/index.tsx
@@ -5,39 +5,47 @@ import {useInputField} from '@simple/react-from'
 interface Props {
   posts: any[];
 }
+import {
+  LiveProvider,
+  LiveEditor,
+  LiveError,
+  LivePreview
+} from 'react-live'
 
 const BlogIndex: NextStatelessComponent<Props> = ({ posts }) => {
-  const [firstName, onChange]: any[] = useInputField({validator: (val: string) => {
-    return val.indexOf('awesome') !== -1
-  }})
+  const [openCode, setCodeBox] = React.useState(false)
+  const scope = {JSON, useInputField};
+  const code = `
+  () =>  {
+    const [firstName, onChange] = useInputField({validator: (val) => {
+      return val.indexOf('awesome') !== -1
+    }})
+    return (<div><input type="text" placeholder="Type deepak is awesome" onChange={firstName.input.onChange} />
+    <div className={firstName.meta.valid ? 'green': 'red'}> Is Form valid?</div>
+    meta data:: { JSON.stringify(firstName.meta, null, 4)}
+    </div>)
+  }
+`
   return (
     <div>
-      <h1>@simple/react-form Demo</h1>
-      <input type="text" placeholder="Type deepak is awesome" value={firstName.input.value} onChange={onChange} onFocus={firstName.input.onFocus} />
-      <div className={firstName.meta.valid ? 'green': 'red'}>
-        Is Form valid?
-      </div>
-      <code className="code">
-        Meta Data
-        <br/>
-        {
-          JSON.stringify(firstName.meta, null, 4)
-        }
-      </code>
+      <h1>@simple/react-form React Live Demo</h1>
+      <LiveProvider code={code} scope={scope} className="live-preview">
+        <LivePreview className="live-preview" />
+        <button onClick={() => setCodeBox(!openCode)}>Toggle Code</button>
+        {openCode && <LiveEditor />}
+        <LiveError />
+      </LiveProvider>
       
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>
-            <Link passHref href={`/${post.id}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <style jsx>{`
+      <style global jsx>{`
         input {
           font-size: 1.5em;
           width: 400px;
+        }
+        textarea {
+          background: #000 !important;
+        }
+        div {
+          margin-bottom: 10px;
         }
         div.green {
           background: #5dc55d;
